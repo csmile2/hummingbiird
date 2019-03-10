02270e522da3455823423c1dc2d09740007d981f
Clayton Smiley <37163784+csmile2@users.noreply.github.com>
2019-03-09 20:48:59 -0600

    Test - Hummingbiird Landing Page
    testing the viability of this landing page

---
+++ dist/precache-manifest.ccd6a7ebd05fa84d2aceddfe18d8c46a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "12d40b48cedd607d6934",
+    "url": "/static/js/main.0b1ca5ab.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "12d40b48cedd607d6934",
+    "url": "/static/css/main.b74024b3.chunk.css"
+  },
+  {
+    "revision": "bda98b26eb93397c6235dda2326aed06",
+    "url": "/index.html"
+  }
+];
